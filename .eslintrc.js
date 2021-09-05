4e9208225e3fa3390be2665afd9250b79c282d9a
nqhung291 <nqhung291@gmail.com>
2021-09-05 11:10:08 +0700

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["react-app", "plugin:prettier/recommended"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+  },
+};
